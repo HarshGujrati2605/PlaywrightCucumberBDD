24e5aed64820fbeae92c17ffbd4f7f3a43c72183
Harsh gujrati <gujratiharsh2011@gmail.com>
2024-05-21 17:03:00 +0530

    Latest code

---
+++ src/main/java/factory/DriverFactory.java
@@ -0,0 +1,58 @@
+package factory;
+
+import java.util.ArrayList;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+
+import utils.WebActions;
+
+public class DriverFactory {
+    public Browser browser;
+    public static BrowserContext context;
+    public static Page page;
+
+    public static ThreadLocal<Page> threadLocalDriver = new ThreadLocal<>(); //For Parallel execution
+    public static ThreadLocal<BrowserContext> threadLocalContext = new ThreadLocal<>();
+
+    //Launches Browser as set by user in config file
+    public Page initDriver(String browserName) {
+        BrowserType browserType = null;
+        ArrayList<String> arguments  = new ArrayList<>();
+        arguments.add("--start-maximized");
+        boolean headless = Boolean.valueOf(WebActions.getProperty("headless"));
+        LaunchOptions launch = new LaunchOptions();
+        switch (browserName) {
+            case "firefox":
+                browserType = Playwright.create().firefox();
+                browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(headless).setArgs(arguments));
+                break;
+            case "chrome":
+                browserType = Playwright.create().chromium();
+                browser = browserType.launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(headless).setArgs(arguments));
+                break;
+            case "webkit":
+                browserType = Playwright.create().webkit();
+                browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(headless).setArgs(arguments));
+                break;
+        }
+        if (browserType == null) throw new IllegalArgumentException("Could not Launch Browser for type" + browserType);
+        context = browser.newContext();
+        //Below line is used to start the trace file
+        context.tracing().start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true).setSources(false));
+        page = context.newPage();
+        threadLocalDriver.set(page);
+        threadLocalContext.set(context);
+        return page;
+    }
+
+    public static synchronized Page getPage() {
+        return threadLocalDriver.get();   
+    }
+
+    public static synchronized BrowserContext getContext() {
+        return threadLocalContext.get(); 
+        
+    }
+
+}
